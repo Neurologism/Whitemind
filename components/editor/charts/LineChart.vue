1c2a2eb7d4a0798124069aa8836ebf8b5f1d7209
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2024-12-18 17:13:03 +0100

    Adjust chart style

--- components/editor/charts/LineChart.vue
+++ components/editor/charts/LineChart.vue
@@ -1,41 +1,36 @@
-<script type="ts" setup>
-import { use } from 'echarts/core';
-import {SVGRenderer} from 'echarts/renderers';
-import { LineChart } from 'echarts/charts';
+<script lang="ts" setup>
+import { use } from "echarts/core";
+import { SVGRenderer } from "echarts/renderers";
+import { LineChart } from "echarts/charts";
+import type { EChartsOption } from "echarts/types/dist/shared";
 import {
   TitleComponent,
   TooltipComponent,
-    GridComponent
-} from 'echarts/components';
-import VChart, { THEME_KEY } from 'vue-echarts';
+  GridComponent,
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
 
-use([
-  SVGRenderer,
-  LineChart,
-  TitleComponent,
-  TooltipComponent,
-  GridComponent
-]);
+use([SVGRenderer, LineChart, TitleComponent, TooltipComponent, GridComponent]);
 
-provide(THEME_KEY, 'dark');
+provide(THEME_KEY, "dark");
 
-const option = ref({
+const option: Ref<EChartsOption> = ref({
   xAxis: {
-
-    type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    type: "category",
+    data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
   },
   yAxis: {
-    type: 'value'
+    type: "value",
   },
   series: [
     {
       data: [820, 932, 901, 934, 1290, 1330, 1320],
-      type: 'line',
-      smooth: true
-    }
-  ]
-})
+      type: "line",
+      smooth: false,
+    },
+  ],
+  backgroundColor: "transparent",
+});
 </script>
 
 <template>
