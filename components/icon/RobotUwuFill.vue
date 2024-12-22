0c4008665b9ab2cee81c0f9c66036bddfabde61b
Emil <me@emilvinu.de>
2024-12-22 15:03:36 +0100

    added icon components

---
+++ components/icon/RobotUwuFill.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+const props = defineProps<{
+  size: string;
+}>();
+</script>
+
+<template>
+  <svg
+    :width="props.size"
+    :height="props.size"
+    viewBox="0 0 24 24"
+    version="1.1"
+    id="svg1"
+    sodipodi:docname="mage--robot-uwu-fill.svg"
+    inkscape:version="1.4 (e7c3feb100, 2024-10-09)"
+    inkscape:export-filename="mage--robot-uwu-fill.png"
+    inkscape:export-xdpi="96"
+    inkscape:export-ydpi="96"
+    xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:svg="http://www.w3.org/2000/svg"
+  >
+    <defs id="defs1" />
+    <sodipodi:namedview
+      id="namedview1"
+      pagecolor="#505050"
+      bordercolor="#eeeeee"
+      borderopacity="1"
+      inkscape:showpageshadow="0"
+      inkscape:pageopacity="0"
+      inkscape:pagecheckerboard="0"
+      inkscape:deskcolor="#505050"
+      inkscape:zoom="33.291667"
+      inkscape:cx="12.015019"
+      inkscape:cy="12"
+      inkscape:window-width="1920"
+      inkscape:window-height="1008"
+      inkscape:window-x="0"
+      inkscape:window-y="0"
+      inkscape:window-maximized="1"
+      inkscape:current-layer="svg1"
+    >
+      <inkscape:page
+        x="0"
+        y="0"
+        width="24"
+        height="24"
+        id="page2"
+        margin="0"
+        bleed="0"
+      />
+    </sodipodi:namedview>
+    <path
+      fill="currentColor"
+      d="M 22.078,8.347 C 21.940096,8.2054369 21.773783,8.0946749 21.59,8.022 V 4.647 c 0,-0.9559996 -1.434,-0.9559996 -1.434,0 V 7.85 h -0.21 C 19.255823,5.5258442 17.120467,3.9314449 14.696,3.93 H 9.427 C 7.0025327,3.9314449 4.8671765,5.5258442 4.177,7.85 H 3.9 V 4.647 c 0,-0.9559996 -1.434,-0.9559996 -1.434,0 V 8.032 C 2.2911256,8.1063454 2.1319698,8.2132411 1.997,8.347 1.6796572,8.667906 1.5011585,9.1006844 1.5,9.552 v 4.896 c -0.00111,0.940447 0.7615532,1.703107 1.702,1.702 h 0.956 c 0.6901765,2.324156 2.8255327,3.918555 5.25,3.92 h 5.183 c 2.424467,-0.0014 4.559823,-1.595844 5.25,-3.92 h 0.955 c 0.940447,0.0011 1.703107,-0.761553 1.702,-1.702 V 9.552 c 0.02,-0.44 -0.131,-0.872 -0.42,-1.205"
+      id="path1"
+      sodipodi:nodetypes="ccssccccccssccccccccccccc"
+    />
+  </svg>
+</template>
