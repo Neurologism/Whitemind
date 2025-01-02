c25e4b92a7faf1cc619fe20b2a287b62668e4d22
Emil <me@emilvinu.de>
2025-01-02 17:36:21 +0100

    improved tutorials page

--- components/tutorials/tutorials.ts
+++ components/tutorials/tutorials.ts
@@ -1,52 +1,62 @@
-export const tutorials = [
+export const tutorialSections = [
   {
-    name: 'Your first neural network',
-    icon: 'mdi:book-open-variant',
-    active: true,
+    label: 'MNIST',
+    tutorials: [
+      {
+        name: 'Your first neural network',
+        icon: 'mdi:book-open-variant',
+        active: true,
+      },
+      {
+        name: 'Datasets and Settings',
+        icon: 'mdi:book-open-variant',
+        active: false,
+      },
+      {
+        name: 'Normalization and Flatten Layer',
+        icon: 'mdi:book-open-variant',
+        active: false,
+      },
+      {
+        name: 'Training a Neural Network',
+        icon: 'mdi:book-open-variant',
+        active: false,
+      },
+      {
+        name: 'Visualizing and Training',
+        icon: 'mdi:book-open-variant',
+        active: false,
+      },
+    ],
   },
   {
-    name: 'Datasets and Settings',
-    icon: 'mdi:book-open-variant',
-    active: false,
-  },
-  {
-    name: 'Normalization and Flatten Layer',
-    icon: 'mdi:book-open-variant',
-    active: false,
-  },
-  {
-    name: 'Training a Neural Network',
-    icon: 'mdi:book-open-variant',
-    active: false,
-  },
-  {
-    name: 'Visualizing and Training',
-    icon: 'mdi:book-open-variant',
-    active: false,
-  },
-  {
-    name: 'Wine Quality',
-    icon: 'mdi:book-open-variant',
-    active: false,
-  },
-  {
-    name: 'Training on Wine Quality Dataset',
-    icon: 'mdi:book-open-variant',
-    active: false,
-  },
-  {
-    name: 'Improving the performance of a neural network',
-    icon: 'mdi:book-open-variant',
-    active: false,
-  },
-  {
-    name: 'How Reliable Is Your Model?',
-    icon: 'mdi:book-open-variant',
-    active: false,
-  },
-  {
-    name: 'Validation and Early Stopping',
-    icon: 'mdi:book-open-variant',
-    active: false,
+    label: 'WINE',
+    tutorials: [
+      {
+        name: 'Wine Quality',
+        icon: 'mdi:book-open-variant',
+        active: false,
+      },
+      {
+        name: 'Training on Wine Quality Dataset',
+        icon: 'mdi:book-open-variant',
+        active: false,
+      },
+      {
+        name: 'Improving the performance of a neural network',
+        icon: 'mdi:book-open-variant',
+        active: false,
+      },
+      {
+        name: 'How Reliable Is Your Model?',
+        icon: 'mdi:book-open-variant',
+        active: false,
+      },
+      {
+        name: 'Validation and Early Stopping',
+        icon: 'mdi:book-open-variant',
+        active: false,
+      },
+    ],
   },
 ];
