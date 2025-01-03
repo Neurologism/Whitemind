9f9a87caec5f7e42309ee461f3039cd8f40a3872
Emil <me@emilvinu.de>
2025-01-03 19:53:48 +0100

    design improvements for account and public settings pages

--- pages/settings/account.vue
+++ pages/settings/account.vue
@@ -10,4 +10,12 @@ definePageMeta({
     change the name that will be displayed for other users, change the
     displayname instead.
   </SettingsInput>
+  <SettingsHeader :weight="2" class="mt-8">Delete account</SettingsHeader>
+  <SettingsText
+    >This will permanently erase all data associated to your account and cannot
+    be undone.
+  </SettingsText>
+  <div>
+    <UButton color="red">Delete your account</UButton>
+  </div>
 </template>
--- pages/settings/public.vue
+++ pages/settings/public.vue
@@ -2,20 +2,31 @@
 definePageMeta({
   layout: 'settings',
 });
+
+const sessionStore = useSessionStore();
 </script>
 <template>
   <SettingsHeader>Public Profile</SettingsHeader>
-  <SettingsInput label="Displayname" placeholder="Kurumi">
-    Name that will be primarily visible to users users. You can change the
-    displayname at any time. It does not need to be unique.
-  </SettingsInput>
-  <SettingsTextarea
-    label="About you"
-    placeholder="I like brainet and raw eggs. "
-  >
-    Tell the world about your interests.
-  </SettingsTextarea>
-  <SettingsInput label="Pronouns" placeholder="he/him"></SettingsInput>
-  <SettingsInput label="Company" placeholder="Eggs Inc."></SettingsInput>
-  <SettingsInput label="Location" placeholder="The Nether"></SettingsInput>
+  <div class="h-4"></div>
+  <ProfilePicture
+    :loading="false"
+    :is-self="true"
+    :pfp-url="sessionStore.hasPfp ? sessionStore.pfpUrl : '/user.svg'"
+  />
+  <div class="h-4"></div>
+  <div class="grow-0">
+    <SettingsInput label="Displayname" placeholder="Kurumi">
+      Name that will be primarily visible to users users. You can change the
+      displayname at any time. It does not need to be unique.
+    </SettingsInput>
+    <SettingsTextarea
+      label="About you"
+      placeholder="I like brainet and raw eggs. "
+    >
+      Tell the world about your interests.
+    </SettingsTextarea>
+    <SettingsInput label="Pronouns" placeholder="he/him"></SettingsInput>
+    <SettingsInput label="Company" placeholder="Eggs Inc."></SettingsInput>
+    <SettingsInput label="Location" placeholder="The Nether"></SettingsInput>
+  </div>
 </template>
