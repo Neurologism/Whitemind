59bda8003409371e52053ec14f839032c43f4c3b
Emil <me@emilvinu.de>
2025-01-03 19:52:10 +0100

    implemented profile picture component

--- components/ProfileComponent.vue
+++ components/ProfileComponent.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import axios from 'axios';
 import type { User } from '~/types/user';
 
 const props = defineProps<{
@@ -8,68 +7,10 @@ const props = defineProps<{
 
 const sessionStore = useSessionStore();
 const userStore = useUserStore();
-const fileInput = ref<HTMLInputElement | null>(null);
 const backmindHost = useRuntimeConfig().public.backmindHost as string;
 const user = ref<User | undefined>(undefined);
 const userLoading = ref(true);
 const pfpUrl = ref<string>('https://whitemind.icinoxis.net/user.svg');
-const hasPfp = ref(true);
-const toast = useToast();
-
-function triggerFileInput() {
-  if (fileInput.value) {
-    fileInput.value.click();
-  }
-}
-
-async function checkForPfp() {
-  const response = await axios
-    .get(pfpUrl.value)
-    .then((response) => (hasPfp.value = response.status === 200))
-    .catch(() => (hasPfp.value = false));
-}
-
-async function handleFileChange(event: Event) {
-  const target = event.target as HTMLInputElement;
-  const file = target.files ? target.files[0] : null;
-
-  if (file) {
-    try {
-      const formData = new FormData();
-      formData.append('pfp', file);
-
-      const response = await sessionStore.fetch('/api/user/upload-pfp', {
-        method: 'POST',
-        body: formData,
-      });
-
-      const data = await response.json();
-
-      if (response.ok) {
-        toast.add({
-          title: 'Profile picture uploaded',
-          icon: 'mdi-check',
-          color: 'green',
-        });
-        window.location.reload();
-      } else {
-        toast.add({
-          title: "Project picture couldn't be uploaded",
-          icon: 'mdi-alert-circle',
-          color: 'red',
-        });
-        console.error(data);
-      }
-    } catch (error) {
-      toast.add({
-        title: "Project picture couldn't be uploaded",
-        icon: 'mdi-alert-circle',
-        color: 'red',
-      });
-      console.error(error);
-    }
-  }
-}
 
 const isSelf = computed(() => {
   return (
@@ -90,7 +31,6 @@ async function loadUser() {
 watch(user, async () => {
   if (user.value) {
     pfpUrl.value = backmindHost + '/api/user/get-pfp/' + user.value._id;
-    checkForPfp();
     userLoading.value = false;
   }
 });
@@ -103,37 +43,14 @@ loadUser();
 </script>
 
 <template>
-  <input
-    ref="fileInput"
-    type="file"
-    name="pfp"
-    accept="image/*"
-    @change="handleFileChange"
-    class="hidden"
-  />
   <div class="w-full">
     <div class="w-2/3 mx-auto flex mt-6">
       <div class="w-52">
-        <LoadingSkeleton :active="userLoading" class="w-52 h-52" rounded="full">
-          <div
-            class="w-52 h-52 rounded-full overflow-hidden relative hover:cursor-pointer"
-            @click="triggerFileInput"
-            v-if="isSelf"
-          >
-            <div
-              class="z-10 absolute w-full h-full bg-black transition-all duration-300 bg-opacity-0 hover:bg-opacity-50 hover:opacity-100 opacity-0"
-            >
-              <UIcon
-                name="fluent:edit-32-filled"
-                class="text-white w-8 h-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
-              />
-            </div>
-            <NuxtImg src="/user.svg" class="w-full h-full" v-if="!hasPfp" />
-            <NuxtImg :src="pfpUrl" class="absolute w-full h-full" v-else />
-          </div>
-          <NuxtImg v-else :src="pfpUrl" class="w-52 h-52 rounded-full" />
-        </LoadingSkeleton>
-
+        <ProfilePicture
+          :loading="userLoading"
+          :pfp-url="pfpUrl"
+          :is-self="isSelf"
+        ></ProfilePicture>
         <div class="mt-6">
           <LoadingSkeleton :active="userLoading" :inline="true" rounded="full">
             <h2 class="text-3xl font-bold inline">
---
+++ components/ProfilePicture.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import axios from 'axios';
+const props = defineProps({
+  loading: Boolean,
+  pfpUrl: { type: String, default: '' },
+  isSelf: { type: Boolean, default: false },
+});
+
+const sessionStore = useSessionStore();
+const fileInput = ref<HTMLInputElement | null>(null);
+const toast = useToast();
+const hasPfp = ref(false);
+
+async function checkForPfp() {
+  if (!props.pfpUrl) {
+    hasPfp.value = false;
+    return;
+  }
+  await axios
+    .get(props.pfpUrl)
+    .then((response) => (hasPfp.value = response.status === 200))
+    .catch(() => (hasPfp.value = false));
+}
+
+function triggerFileInput() {
+  if (fileInput.value) {
+    fileInput.value.click();
+  }
+}
+
+async function handleFileChange(event: Event) {
+  const target = event.target as HTMLInputElement;
+  const file = target.files ? target.files[0] : null;
+
+  if (file) {
+    try {
+      const formData = new FormData();
+      formData.append('pfp', file);
+
+      const response = await sessionStore.fetch('/api/user/upload-pfp', {
+        method: 'POST',
+        body: formData,
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        toast.add({
+          title: 'Profile picture uploaded',
+          icon: 'mdi-check',
+          color: 'green',
+        });
+        window.location.reload();
+      } else {
+        toast.add({
+          title: "Project picture couldn't be uploaded",
+          icon: 'mdi-alert-circle',
+          color: 'red',
+        });
+        console.error(data);
+      }
+    } catch (error) {
+      toast.add({
+        title: "Project picture couldn't be uploaded",
+        icon: 'mdi-alert-circle',
+        color: 'red',
+      });
+      console.error(error);
+    }
+  }
+}
+
+watch(() => props.pfpUrl, checkForPfp);
+
+onMounted(() => {
+  checkForPfp();
+});
+</script>
+
+<template>
+  <input
+    ref="fileInput"
+    type="file"
+    name="pfp"
+    accept="image/*"
+    @change="handleFileChange"
+    class="hidden"
+    style="width: 0; height: 0"
+  />
+  <LoadingSkeleton :active="loading" :class="`w-52 h-52`" rounded="full">
+    <div
+      :class="`w-52 h-52 rounded-full overflow-hidden relative hover:cursor-pointer`"
+      @click="triggerFileInput"
+      v-if="isSelf"
+    >
+      <div
+        class="z-10 absolute w-full h-full bg-black transition-all duration-300 bg-opacity-0 hover:bg-opacity-50 hover:opacity-100 opacity-0"
+      >
+        <UIcon
+          name="fluent:edit-32-filled"
+          class="text-white w-8 h-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
+        />
+      </div>
+      <NuxtImg
+        :src="hasPfp ? props.pfpUrl : '/user.svg'"
+        class="w-full h-full"
+      />
+    </div>
+    <NuxtImg
+      v-else
+      :src="hasPfp ? props.pfpUrl : '/user.svg'"
+      :class="`w-52 h-52 rounded-full`"
+    />
+  </LoadingSkeleton>
+</template>
