7f4804dc2cfe76a5f05bead39f34b90646596cb3
Emil <me@emilvinu.de>
2025-01-03 17:44:58 +0100

    added settings layout

---
+++ layouts/settings.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts"></script>
+
+<template>
+  <AppHeader />
+  <div class="h-16"></div>
+  <div class="w-2/3 mx-auto flex flex-row mt-8">
+    <div class="flex flex-col w-40 mr-10">
+      <UButton
+        color="gray"
+        variant="ghost"
+        to="/settings/public"
+        icon="mdi:user-outline"
+        >Public Profile</UButton
+      >
+      <UButton
+        color="gray"
+        variant="ghost"
+        to="/settings/account"
+        icon="i-heroicons-cog-6-tooth"
+        >Account</UButton
+      >
+      <UButton
+        color="gray"
+        variant="ghost"
+        to="/settings/emails"
+        icon="mdi:email-outline"
+        >Mail & Phone</UButton
+      >
+      <UButton
+        color="gray"
+        variant="ghost"
+        to="/settings/authentication"
+        icon="mdi:key-outline"
+        >Authentication</UButton
+      >
+    </div>
+    <div class="w-full flex flex-col space-y-4">
+      <slot />
+    </div>
+  </div>
+</template>
