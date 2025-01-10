2501bbb7396b802e26bc4de597d4607cc75738f5
Emil <me@emilvinu.de>
2025-01-10 20:12:48 +0100

    add icon centering for editor tabs

--- components/editor/TabsBar.vue
+++ components/editor/TabsBar.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
+import { navigateToSubroute } from '~/utility/navigateToSubroute';
 const route = useRoute();
-
-function navigateToSubroute(text: string) {
-  console.log(route.fullPath);
-  navigateTo(route.fullPath.split('/').slice(0, 3).join('/') + '/' + text);
-}
 
 const tabs = [
   {
@@ -28,34 +24,33 @@ const tabs = [
       class="bg-slate-800 flex flex-row mx-auto pointer-events-auto z-10 border-b-2 border-x-2 border-slate-600 rounded-b-full px-6 select-none"
     >
       <div
-        @click="navigateToSubroute('editor')"
+        @click="navigateToSubroute(route.fullPath, 3, 'editor')"
         variant="ghost"
         color="gray"
         size="xl"
-        class="h-full cursor-pointer text-slate-200 hover:text-white transition-all duration-100 py-2 px-4"
+        class="h-full cursor-pointer text-slate-200 hover:text-white transition-all duration-100 py-2 px-4 flex flex-row items-center"
       >
-        <UIcon name="carbon:model-builder"></UIcon>
+        <UIcon name="carbon:model-builder" class="mr-1"></UIcon>
         Editor
       </div>
       <div
-        @click="navigateToSubroute('models')"
+        @click="navigateToSubroute(route.fullPath, 3, 'models')"
         variant="ghost"
         color="gray"
         size="xl"
-        class="h-full cursor-pointer text-slate-200 hover:text-white transition-all duration-100 py-2 px-4"
+        class="h-full cursor-pointer text-slate-200 hover:text-white transition-all duration-100 py-2 px-4 flex flex-row items-center"
       >
-        <UIcon name="mdi:cube"></UIcon>
+        <UIcon name="mdi:cube" class="mr-1"></UIcon>
         Models
       </div>
       <div
-        @click="navigateToSubroute('settings')"
+        @click="navigateToSubroute(route.fullPath, 3, 'settings')"
         variant="ghost"
         color="gray"
         size="xl"
-        class="h-full cursor-pointer text-slate-200 hover:text-white transition-all duration-100 py-2 px-4"
+        class="h-full cursor-pointer text-slate-200 hover:text-white transition-all duration-100 py-2 px-4 flex flex-row items-center"
       >
-        <UIcon name="mdi:settings-outline"></UIcon>
-        Settings
+        <UIcon name="mdi:settings-outline" class="mr-1"></UIcon> Settings
       </div>
     </div>
   </div>
