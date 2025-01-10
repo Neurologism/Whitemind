20342ca2a6b138a6034df69b2a24a351acf99cc6
Emil <me@emilvinu.de>
2025-01-10 22:03:24 +0100

    add settings select component

---
+++ components/settings/Select.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    label: string;
+    disabled?: boolean;
+    setWidth?: boolean;
+    options: string[];
+  }>(),
+  {
+    disabled: false,
+    setWidth: true,
+  }
+);
+
+// const model = defineModel() as Ref<string>;
+</script>
+<template>
+  <div :class="{ 'w-[32rem]': props.setWidth }">
+    <h4 class="text-md font-semibold mb-2" v-if="props.label">
+      {{ props.label }}
+    </h4>
+    <USelect :options="props.options" :disabled="props.disabled" />
+    <p class="text-sm text-gray-400">
+      <slot></slot>
+    </p>
+  </div>
+</template>
