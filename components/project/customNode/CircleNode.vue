63c0d5bef0ff138350625b172f4be8b1af1917f5
lxorb <101118850+lxorb@users.noreply.github.com>
2025-02-12 08:56:06 +0100

    draft circle node

---
+++ components/project/customNode/CircleNode.vue
@@ -0,0 +1,181 @@
+<script setup lang="ts">
+import { Position, useNodesData } from '@vue-flow/core';
+import NodeValueEditor from '~/components/project/customNode/typeEditors/NodeValueEditor.vue';
+import LineChart from '~/components/project/charts/LineChart.vue';
+import '@vue-flow/node-resizer/dist/style.css';
+import ClassicHandle from '~/components/project/customNode/ClassicHandle.vue';
+
+defineEmits(['node-contextmenu']);
+
+const props = defineProps({
+  nodeId: {
+    type: String,
+    required: true,
+  },
+});
+
+const projectStore = useProjectStore();
+
+const nodesData = useNodesData(props.nodeId);
+const shapeData = projectStore.editorConfig.getCustomNodeConfig(
+  nodesData.value!.type
+)!;
+const shapeGroupData = projectStore.editorConfig.getNodeGroup(
+  nodesData.value!.type
+)!;
+
+const chartComponentsByIdentifier: Record<string, any> = {
+  'line-chart': LineChart,
+};
+
+// @ts-ignore - the value is set here initialy
+nodesData.value.data.isExpanded ??= true;
+// @ts-ignore - the value is set here initialy
+nodesData.value.data.showVisConfigs ??= false;
+
+function clickIcons() {
+  if (shapeGroupData.group_identifier !== 'visualizer') {
+    nodesData.value!.data.isExpanded = !nodesData.value!.data.isExpanded;
+  }
+  if (shapeGroupData.group_identifier === 'visualizer') {
+    nodesData.value!.data.showVisConfigs =
+      !nodesData.value!.data.showVisConfigs;
+  }
+}
+</script>
+<template>
+  <div
+    class="h-full w-full text-zinc-50 rounded bg-bg-3 flex flex-col border-r-2 border-2 border-accent-8 shadow-2xl"
+    @contextmenu.prevent="$emit('node-contextmenu', props.nodeId)"
+    :style="{
+      width: `${shapeGroupData.default_width}px`,
+    }"
+  >
+    <div
+      class="flex justify-between items-center font-mono pt-0.5 pb-0.5 cursor-all-scroll rounded-t relative"
+      :class="{
+        'pl-1.5': shapeData.hideInput !== true,
+        'pr-3': shapeData.hideOutput !== true,
+        'pr-1': shapeData.hideOutput === true,
+      }"
+      :style="{
+        backgroundColor: shapeGroupData.color,
+      }"
+    >
+      <ClassicHandle
+        v-if="shapeData.hideInput !== true"
+        :constraints="shapeData.inputConstraints"
+        :handle-id="`in-${props.nodeId}`"
+        :is-input="true"
+        :position="Position.Left"
+        :shape-data="shapeData"
+        :shape-group-data="shapeGroupData"
+      />
+      <ClassicHandle
+        v-if="shapeData.hideOutput !== true"
+        :constraints="shapeData.outputConstraints"
+        :handle-id="`out-${props.nodeId}`"
+        :is-input="false"
+        :position="Position.Right"
+        :shape-data="shapeData"
+        :shape-group-data="shapeGroupData"
+      />
+      <div
+        class="flex justify-between items-center p-1 cursor-pointer"
+        @click="clickIcons"
+      >
+        <UIcon
+          v-if="shapeGroupData.group_identifier !== 'visualizer'"
+          name="material-symbols:expand-more"
+          :style="{
+            transform: nodesData!.data.isExpanded
+              ? 'rotate(0deg)'
+              : 'rotate(270deg)',
+          }"
+        />
+        <UIcon :name="shapeGroupData.icon" />
+        <UIcon
+          v-if="shapeGroupData.group_identifier === 'visualizer'"
+          :name="
+            nodesData!.data.showVisConfigs
+              ? 'mdi-settings'
+              : 'mdi-settings-outline'
+          "
+        />
+      </div>
+      <span class="font-semibold">{{ shapeData.name }}</span>
+    </div>
+    <div class="w-full h-full nodrag nowheel cursor-default">
+      <div :class="`${nodesData!.data.isExpanded ? 'mt-0.5 mb-1' : null}`">
+        <div v-for="(shapeDefinition, key) in shapeData.data">
+          <div
+            v-if="shapeDefinition.type === 'id'"
+            class="mt-0.5 mb-0.5 p-0.5"
+            :class="{
+              'ml-3 rounded-l-sm pr-0': shapeData.data[key].invertPosition
+                ? !(shapeData.data[key].flowOrientation === 'output')
+                : shapeData.data[key].flowOrientation === 'output',
+              'mr-3 rounded-r-sm pl-0': shapeData.data[key].invertPosition
+                ? !(shapeData.data[key].flowOrientation === 'input')
+                : shapeData.data[key].flowOrientation === 'input',
+              'justify-end flex': shapeData.data[key].invertPosition
+                ? !(shapeData.data[key].flowOrientation === 'output')
+                : shapeData.data[key].flowOrientation === 'output',
+            }"
+          >
+            <div class="font-mono text-sm relative">
+              <span class="bg-bg-3 pr-2.5 pl-2.5 p-0.5 rounded font-mono">{{
+                key
+              }}</span>
+              <ClassicHandle
+                :constraints="shapeDefinition.constraints"
+                :handle-id="`val-${key}-${props.nodeId}`"
+                :key="`${key}-${props.nodeId}`"
+                :is-input="shapeData.data[key].flowOrientation! === 'input'"
+                :position="
+                  (
+                    shapeData.data[key].invertPosition
+                      ? !(shapeData.data[key].flowOrientation === 'input')
+                      : shapeData.data[key].flowOrientation === 'input'
+                  )
+                    ? Position.Left
+                    : Position.Right
+                "
+                :shape-data="shapeData"
+                :shape-group-data="shapeGroupData"
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          v-if="nodesData!.data.isExpanded && !nodesData!.data.showVisConfigs"
+          class="mr-1 ml-1 mb-1 bg-bg-4"
+          style="height: 1px"
+        />
+        <div
+          v-if="nodesData!.data.isExpanded && !nodesData!.data.showVisConfigs"
+          v-for="(shapeDefinition, key) in shapeData.data"
+          class="grid grid-cols-1 mb-0.5 mt-1 items-center justify-between"
+        >
+          <NodeValueEditor
+            :key="key"
+            v-if="shapeDefinition.type !== 'id'"
+            :node-id="props.nodeId"
+            :param-name="key"
+            :shape-definition="shapeDefinition"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.resize-arrow {
+  position: absolute;
+  left: -0.5px;
+  top: -0.5px;
+  font-size: 1rem;
+  color: black;
+}
+</style>
