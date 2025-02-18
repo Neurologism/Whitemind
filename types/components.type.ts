164e6f458e88d261e9fa8fb05b4d8738815c5f14
lxorb <101118850+lxorb@users.noreply.github.com>
2025-02-18 18:21:16 +0100

    add components type

---
+++ types/components.type.ts
@@ -0,0 +1,16 @@
+import type { Node } from '@vue-flow/core';
+import type { Edge } from './edge.type';
+
+export interface Components extends OptionalExports {
+  nodes: Node[];
+  edges: Edge[];
+  viewport: {
+    x: number;
+    y: number;
+    zoom: number;
+  };
+}
+
+export interface OptionalExports {
+  perceptrons?: Record<string, any>;
+}
