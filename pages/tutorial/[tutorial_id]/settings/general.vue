ff19f6ddf907ed80a4a990e5b929e2e1e313855e
lxorb <101118850+lxorb@users.noreply.github.com>
2025-03-22 13:06:56 +0100

    add tutorial settings

---
+++ pages/tutorial/[tutorial_id]/settings/general.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'tutorial',
+});
+
+const route = useRoute();
+const tutorialStore = useTutorialStore();
+
+tutorialStore.tutorialId = route.params.tutorial_id as string;
+</script>
+
+<template>
+  <div class="w-screen bg-bg-2" style="min-height: calc(100vh - 4rem)">
+    <SettingsBase variant="tutorial">
+      <SettingsHeader>General</SettingsHeader>
+      <div class="text-xl text-text-2">Nothing here yet.</div>
+    </SettingsBase>
+  </div>
+</template>
---
+++ pages/tutorial/[tutorial_id]/settings/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { navigateToSubroute } from '~/utility/navigateToSubroute';
+
+definePageMeta({
+  layout: 'tutorial',
+});
+
+const route = useRoute();
+const tutorialStore = useTutorialStore();
+
+tutorialStore.tutorialId = route.params.tutorial_id as string;
+
+navigateToSubroute(route.fullPath, 5, 'general');
+</script>
+
+<template>
+  <div class="w-screen bg-bg-2" style="min-height: calc(100vh - 4rem)">
+    <SettingsBase variant="tutorial"> </SettingsBase>
+  </div>
+</template>
