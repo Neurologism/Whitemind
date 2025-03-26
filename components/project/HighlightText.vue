4a339dd2b199b6772faade4b0412aa4ddf0eb89b
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2025-03-26 15:29:07 +0100

    fix: improve text highlighting by refining regex and adjusting styles

--- components/project/HighlightText.vue
+++ components/project/HighlightText.vue
@@ -9,7 +9,7 @@ const projectStore = useProjectStore();
 
 function splitAtMultiple(text: string, nodes: NodeDefinition[]) {
   const justNames = nodes.map((node) => node.name);
-  const regex = new RegExp(`(${justNames.join('|')})`, 'gi');
+  const regex = new RegExp(`(${justNames.join('|')})(?![a-zA-Z])`, 'gi');
   return text.split(regex).map((part) => {
     const node = nodes.find(
       (node) => node.name.toLowerCase() === part.toLowerCase()
@@ -24,7 +24,10 @@ const highlightedParts = computed(() => {
     icon?: string;
     color?: string;
   }[] = [];
-  const split = splitAtMultiple(props.text, projectStore.editorConfig.allNodes);
+  const split = splitAtMultiple(
+    props.text + ' lorem ips datasets',
+    projectStore.editorConfig.allNodes
+  );
   for (const part of split) {
     if (part.node) {
       let group = projectStore.editorConfig.getNodeGroup(part.node.type);
@@ -48,15 +51,17 @@ const highlightedParts = computed(() => {
   <span
     v-for="(part, index) in highlightedParts"
     :key="index"
-    class="font-mono text-sm leading-none"
+    class="font-mono text-sm leading-none p-0 m-0"
     :class="{
       'brightness-150 font-semibold': part.color !== undefined,
     }"
     :style="{
       color: part.color,
     }"
-  >
-    <icon v-if="part.icon" :name="part.icon" class="inline-block w-3 h-3" />
-    {{ part.text }}
+    ><icon
+      v-if="part.icon"
+      :name="part.icon"
+      class="inline-block w-3 h-3 mr-1"
+    />{{ part.text.trimStart() }}
   </span>
 </template>
