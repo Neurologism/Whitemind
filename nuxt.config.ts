a3c713fd995c4206fa0f35eeb81fc417e9763f27
lxorb <me@emilvinu.de>
2026-08-31 10:51:40 +0200

    deploy to workers instead of pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   devServer: {
     port: 8000,
@@ -8,6 +8,9 @@ export default defineNuxtConfig({
   routeRules: {
     '/project/**': { ssr: false },
     '/tutorial/**': { ssr: false },
+  },
+  nitro: {
+    preset: 'cloudflare_module',
   },
   modules: [
     '@nuxt/ui',
